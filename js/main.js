7549abc4348340a0ace70cb2a6622068ddcfa888
Your Name <marina7953042@yandex.ru>
2020-02-02 12:13:45 +0500

    Добавлена карточка объявления

--- js/main.js
+++ js/main.js
@@ -213,4 +213,30 @@ var addPinsOnMap = function (pins) {
   mapPinsElement.appendChild(fragment);
 };
 
-addPinsOnMap(generateOffers(numberPins));
+/**
+* generates an ad with the necessary characteristics
+* @param {Object} card the ad of array
+*/
+var generateCard = function (card) {
+  var cardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.map__card');
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + 'Р/ночь';
+  cardElement.querySelector('.popup__type').textContent = card.offer.type;
+  cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнат(-ы) для ' + card.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  cardElement.querySelector('.popup__features').textContent = card.offer.features;
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
+  cardElement.querySelector('.popup__photos').src = card.offer.photos;
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+
+  map.insertBefore(cardElement, document.querySelector('.map__filters-container'));
+};
+
+// check the display on the page (temporarily)
+var pinsArray = generateOffers(numberPins);
+addPinsOnMap(pinsArray);
+generateCard(pinsArray[0]);
